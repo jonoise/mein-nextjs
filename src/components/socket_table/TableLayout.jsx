9d7b9b9ee3cd81a8cc15aadbf453f37eaeaa4e23
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-04 10:24:23 -0600

    add TableLayout

--- src/components/socket_table/TableLayout.jsx
+++ src/components/socket_table/TableLayout.jsx
@@ -1,7 +1,36 @@
-import { VStack } from '@chakra-ui/react'
+import { VStack, useToast } from '@chakra-ui/react'
+import { useState } from 'react'
+import { io } from 'socket.io-client'
+
+const socket = io('http://localhost:4000')
 
 const TableLayout = () => {
-  return <VStack minH="100vh" bg="facebook.500"></VStack>
+  const [user, setUser] = useState(null)
+  const toast = useToast()
+
+  socket.on('connect', () => {
+    socket.emit('new-user', tableInstance)
+  })
+
+  socket.on('new-user', (user) => {
+    toast({
+      id: user,
+      title: `${user} ingresó a la mesa.`,
+      position: 'top',
+    })
+  })
+
+  const addUser = () => {}
+  return (
+    <VStack minH="100vh" bg="facebook.500">
+      <button
+        onClick={addUser}
+        style={{ padding: '10px', background: 'yellow' }}
+      >
+        log
+      </button>
+    </VStack>
+  )
 }
 
 export default TableLayout

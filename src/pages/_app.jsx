8c799d7bdc6fe6b8f21c6f8920bbc11adafd9944
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-05 23:40:12 -0600

    remove redux from app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,20 +1,15 @@
-import { Provider as ReduxProvider } from 'react-redux'
 import { Provider as NextAuthProvider } from 'next-auth/client'
 import { ChakraProvider, CSSReset } from '@chakra-ui/react'
-import { store } from '../app/store'
 import '../styles/globals.css'
-import Navbar from '../components/navbar/Navbar'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     // <ReduxProvider store={store}>
     <NextAuthProvider session={pageProps.session}>
-      <ReduxProvider store={store}>
-        <ChakraProvider>
-          <CSSReset />
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </ReduxProvider>
+      <ChakraProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </NextAuthProvider>
     // </ReduxProvider>
   )

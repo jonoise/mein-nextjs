1765819a1abdf9951b064e79fda05f89d5983184
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-06 00:48:04 -0600

    add rest to nameModal

--- src/components/socket_table/modals/NameModal.jsx
+++ src/components/socket_table/modals/NameModal.jsx
@@ -15,6 +15,8 @@ import {
   FormLabel,
   IconButton,
   Text,
+  Flex,
+  Image,
 } from '@chakra-ui/react'
 import { AiOutlineUser } from 'react-icons/ai'
 import { useRef, useState } from 'react'
@@ -30,6 +32,7 @@ const NameModal = () => {
 
   const users = useTableStore((state) => state.users)
   const addUser = useTableStore((state) => state.addUser)
+  const restaurant = useTableStore((state) => state.restaurant)
 
   const handleNameChange = (e) => {
     setUserName(e.target.value)
@@ -55,7 +58,7 @@ const NameModal = () => {
     if (userName.length < 1) {
       return
     }
-
+    console.log(restaurant)
     addUser(newUser)
     socket.emit('addUser', instance_uuid, newUser)
     setIsOpen(false)
@@ -79,7 +82,19 @@ const NameModal = () => {
         <DrawerOverlay />
         <DrawerContent bg={styles.dimWhite} color="black">
           <DrawerHeader borderBottomWidth="1px" borderColor="black">
-            MESA {tableNumber}
+            <Flex w="full" h="full" justify="space-between" align="center">
+              MESA {tableNumber}{' '}
+              <Button
+                onClick={addUserHandler}
+                style={{
+                  padding: '10px',
+                  background: styles.yellow,
+                  color: styles.black,
+                }}
+              >
+                Sentarme
+              </Button>
+            </Flex>
           </DrawerHeader>
 
           <DrawerBody>
@@ -94,40 +109,37 @@ const NameModal = () => {
                   value={userName}
                 />
               </VStack>
-              <Stack>
+              <Stack fontSize="16px">
                 <Text color={styles.black}>
                   -Al ingresar tu nombre nuestro equipo de meseros podrá darte
                   una atención más personalizada. 😊
                 </Text>
                 <Text color={styles.black} fontWeight="semibold">
-                  Además:
+                  Además podrás:
                 </Text>
                 <Text color={styles.black}>
-                  🥗 Podrás compartir sugerencias con las demás personas de la
+                  🔔 Llamar a un mesero con el toque de un botón.
+                </Text>
+                <Text color={styles.black}>
+                  💵 Dividir la cuenta de manera individual si lo deseas.
+                </Text>
+                <Text color={styles.black}>
+                  🥗 Compartir sugerencias en tiempo real con los miembros de la
                   mesa.
                 </Text>
                 <Text color={styles.black}>
-                  💵 Dividir la cuenta por persona.
-                </Text>
-                <Text color={styles.black}>
-                  🥘 Aceptar recomendaciones de otros.
+                  🥘 Aceptar recomendaciones de las demás personas de la mesa.
                 </Text>
               </Stack>
             </Stack>
           </DrawerBody>
 
-          <DrawerFooter borderColor="black" borderTopWidth="1px">
-            <Button
-              w="full"
-              onClick={addUserHandler}
-              style={{
-                padding: '10px',
-                background: styles.yellow,
-                color: styles.black,
-              }}
-            >
-              Entrar a la mesa
-            </Button>
+          <DrawerFooter
+            borderColor="none"
+            borderTopWidth="1px"
+            justifyContent="center"
+          >
+            {restaurant && <Image w="120px" src={restaurant.logo} />}
           </DrawerFooter>
         </DrawerContent>
       </Drawer>

30b1ad89e5934db7f563aafb85133237610d4a6c
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-07 14:37:54 -0600

    add MeinBody

--- src/components/socket_table/menu/Mein.jsx
+++ src/components/socket_table/menu/Mein.jsx
@@ -1,14 +1,14 @@
 import MeinHeader from './MeinHeader'
 import MeinNav from './MeinNav'
 import MeinCategories from './MeinCategories'
-import MeinDishList from './dishes/MeinDishList'
+import MeinBody from './MeinBody'
 const Mein = () => {
   return (
     <div>
       <MeinNav />
       <MeinHeader />
       <MeinCategories />
-      <MeinDishList />
+      <MeinBody />
     </div>
   )
 }
---
+++ src/components/socket_table/menu/MeinBody.jsx
@@ -0,0 +1,20 @@
+import { Stack, Text } from '@chakra-ui/react'
+import useTableStore from '../tableStore'
+import RestaurantSpecifics from './dishes/RestaurantSpecifics'
+import CategoryItems from './dishes/CategoryItems'
+const MeinBody = () => {
+  const stateCategory = useTableStore((state) => state.stateCategory)
+  return (
+    <Stack minH="100vh" w="full" bg="white" p="2">
+      {stateCategory ? (
+        // Desplegar items.
+        <CategoryItems stateCategory={stateCategory} />
+      ) : (
+        // Desplegar todo el menu
+        <RestaurantSpecifics />
+      )}
+    </Stack>
+  )
+}
+
+export default MeinBody

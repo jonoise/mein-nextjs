b1f827a0a3a3f3af40908ede87060f51090ba458
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-07 14:38:54 -0600

    add menu categories slider

--- src/components/socket_table/menu/MeinCategories.jsx
+++ src/components/socket_table/menu/MeinCategories.jsx
@@ -4,8 +4,10 @@ import { useState } from 'react'
 import SwiperCore, { Navigation, Scrollbar, A11y } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import useTableStore from '../tableStore'
+
 const MeinCategories = () => {
   const menu = useTableStore((state) => state.menu)
+  const setStateCategory = useTableStore((state) => state.setStateCategory)
   SwiperCore.use([Navigation, Scrollbar, A11y])
 
   const [swiperSlideWidth, setSwiperSlideWidth] = useState(0)
@@ -13,37 +15,45 @@ const MeinCategories = () => {
     setSwiperSlideWidth(swiper.width)
   }
 
+  const handleSetStateCategory = (category) => {
+    setStateCategory(category)
+  }
+
   return (
-    <Flex p="2">
-      <Swiper
-        loop={true}
-        slidesPerView={2}
-        spaceBetween={20}
-        onSwiper={(swiper) => onSwiper(swiper)}
-        onSlideChange={() => console.log('slide change')}
-        className="mySwiper"
-      >
-        {menu && (
-          <HStack spacing="2">
-            {menu._categories.map((category) => {
-              return (
-                <SwiperSlide key={category.id}>
-                  <Flex
-                    bg="green.100"
-                    p="2"
-                    justify="space-evenly"
-                    borderRadius="md"
-                  >
-                    <Image src={categoryIcon[category.name]} w="30px" />
-                    <Text fontWeight="semibold">{category.name}</Text>
-                  </Flex>
-                </SwiperSlide>
-              )
-            })}
-          </HStack>
-        )}
-      </Swiper>
-    </Flex>
+    <>
+      <Text pl="2">Desliza para más</Text>
+      <Flex p="2">
+        <Swiper
+          loop={true}
+          slidesPerView={2}
+          spaceBetween={20}
+          onSwiper={(swiper) => onSwiper(swiper)}
+          onSlideChange={() => console.log('slide change')}
+          className="mySwiper"
+        >
+          {menu && (
+            <HStack spacing="2">
+              {menu._categories.map((category) => {
+                return (
+                  <SwiperSlide key={category.id}>
+                    <Flex
+                      bg="green.100"
+                      p="2"
+                      justify="space-evenly"
+                      borderRadius="md"
+                      onClick={() => handleSetStateCategory(category.name)}
+                    >
+                      <Image src={categoryIcon[category.name]} w="30px" />
+                      <Text fontWeight="semibold">{category.name}</Text>
+                    </Flex>
+                  </SwiperSlide>
+                )
+              })}
+            </HStack>
+          )}
+        </Swiper>
+      </Flex>
+    </>
   )
 }
 

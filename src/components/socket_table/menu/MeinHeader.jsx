252e56a3583d524e28f8efb761c9975af7b674bb
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-07 14:39:09 -0600

    add MeinHeader

--- src/components/socket_table/menu/MeinHeader.jsx
+++ src/components/socket_table/menu/MeinHeader.jsx
@@ -5,12 +5,14 @@ const MeinHeader = () => {
   const restaurant = useTableStore((state) => state.restaurant)
 
   return (
-    <VStack pt="75px">
-      <Image src={restaurant && restaurant.logo} w="140px" />
-      <Text fontSize="24px" fontWeight="semibold" color="#24423B">
-        Menu
-      </Text>
-    </VStack>
+    restaurant && (
+      <VStack pt="75px">
+        <Image src={restaurant.logo} w="140px" />
+        <Text fontSize="24px" fontWeight="semibold" color="#24423B">
+          Menu
+        </Text>
+      </VStack>
+    )
   )
 }
 

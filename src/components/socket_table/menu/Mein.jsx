eb349932702e138cca67cab6ed7d5ed5583fcc5f
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-07 14:36:29 -0600

    add graphics

--- src/components/socket_table/menu/Mein.jsx
+++ src/components/socket_table/menu/Mein.jsx
@@ -1,12 +1,14 @@
 import MeinHeader from './MeinHeader'
 import MeinNav from './MeinNav'
 import MeinCategories from './MeinCategories'
+import MeinDishList from './dishes/MeinDishList'
 const Mein = () => {
   return (
     <div>
       <MeinNav />
       <MeinHeader />
       <MeinCategories />
+      <MeinDishList />
     </div>
   )
 }

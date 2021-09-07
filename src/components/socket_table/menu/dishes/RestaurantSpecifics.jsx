b844cc148929d20a93c95586652f423208418507
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-07 14:42:32 -0600

    add rest welcome

---
+++ src/components/socket_table/menu/dishes/RestaurantSpecifics.jsx
@@ -0,0 +1,21 @@
+import axiosWithJWT from '../../../../lib/axios'
+import useTableStore from '../../tableStore'
+import { Text, VStack } from '@chakra-ui/react'
+import SingleDish from './SingleDish'
+
+const MenuItems = () => {
+  const restaurant = useTableStore((state) => state.restaurant)
+  const menu = useTableStore((state) => state.menu)
+  return (
+    restaurant && (
+      <VStack textAlign="center">
+        <Text fontSize="20px" fontWeight="semibold">
+          Bienvenidos a {restaurant.name}
+        </Text>
+        <Text>{restaurant.welcome_message.trim()}</Text>
+      </VStack>
+    )
+  )
+}
+
+export default MenuItems

dcc9b15385c72c9023da4701cadb7998c0c8e59f
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-10 00:31:24 -0600

    fixed credentialsManager

--- src/lib/credentialsManager.js
+++ src/lib/credentialsManager.js
@@ -7,40 +7,56 @@ const REGISTER_URL = `${process.env.API_URL}/users/` || `${localAPIUrl}/users/`
 const LOGIN_URL = `${process.env.API_URL}/users/login/` || `${localAPIUrl}/users/login/`
 
 export const credentialsManager = async (credentials) => {
-    switch (credentials.type_of) {
-        case 'register':
-            const register_res = await axios.post(REGISTER_URL, {
-                password: credentials.password,
-                email: credentials.email
-            })
-            const {
-                account, message
-            } = register_res.data
 
-            if (message === 'created') {
-                return {
-                    ...account,
-                    error: null
-                }
-            } else if (message === 'already_exists') {
-                return {
-                    error: {
-                        message,
-                        query: '⛔ Ya existe un usuario asociado a ese email.'
-                    }
-                }
-            }
-            break
+    if (credentials.type_of === 'login') {
+        try {
 
-        case 'login':
             const login_res = await axios.post(LOGIN_URL, {
                 password: credentials.password,
                 email: credentials.email
             })
+            let {
+                account,
+            } = login_res.data
 
-            return false
+            return {
+                ...account,
+                error: null
+            }
+        } catch (error) {
+            return {
+                error: {
+                    message: 'invalid_credentials',
+                    query: '⛔ Las credenciales no son válidas.'
+                }
+            }
+        }
 
-        default:
-            break;
     }
+
+    if (credentials.type_of === 'register') {
+
+        const register_res = await axios.post(REGISTER_URL, {
+            password: credentials.password,
+            email: credentials.email
+        })
+        let {
+            account,
+            message
+        } = register_res.data
+
+        if (message === 'already_exists') {
+            return {
+                error: {
+                    message,
+                    query: '⛔ Ya existe un usuario asociado a ese email.'
+                }
+            }
+        }
+        return {
+            ...account,
+            error: null
+        }
+    }
+
 }

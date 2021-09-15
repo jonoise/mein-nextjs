bafa44a247155f39290a252a4ff26e7528db1257
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-14 18:12:47 -0600

    add generic head

---
+++ src/components/generichead/GenericHead.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+const GenericHead = ({ title, description, image, type, url }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content={type} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+    </Head>
+  )
+}
+
+export default GenericHead

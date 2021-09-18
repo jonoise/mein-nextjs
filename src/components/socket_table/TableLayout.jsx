be810a005693e652a40c066b6bcd57e612178421
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-17 22:17:54 -0600

    add genericHead to socket_table

--- src/components/socket_table/TableLayout.jsx
+++ src/components/socket_table/TableLayout.jsx
@@ -3,10 +3,12 @@ import BotNav from './nav/BotNav'
 import Menu from './menu/Mein'
 import socket from './socketConnect'
 import TopNav from './nav/TopNav'
-
+import GenericHead from '../generichead/GenericHead'
+import useTableStore from '../../stores/tableStore'
 const TableLayout = () => {
+  const rest = useTableStore((state) => state.restaurant)
+  const tableNumber = useTableStore((state) => state.tableNumber)
   const toast = useToast()
-
   socket.on('new-user', (user) => {
     toast({
       id: user,
@@ -17,6 +19,7 @@ const TableLayout = () => {
 
   return (
     <Flex minH="100vh" direction="column">
+      {rest && <GenericHead title={`Mesa ${tableNumber} del ${rest.name}`} />}
       <TopNav />
       <Menu />
       <BotNav />

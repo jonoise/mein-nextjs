bcd186fd8204d9e710d609cc2507ef6b1915c8b1
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-17 21:49:31 -0600

    add rest layout components

--- src/components/dashboard_restaurant/home/RestaurantDashboardLayout.jsx
+++ src/components/dashboard_restaurant/home/RestaurantDashboardLayout.jsx
@@ -2,17 +2,20 @@ import DashboardApp from '../sidebar_restaurant/App'
 import { VStack, Divider } from '@chakra-ui/react'
 import { useEffect } from 'react'
 import useGeneralDashboardStore from '../../../stores/generalDashboardStore'
-import App from '../sidebar_restaurant/App'
+import RestaurantDetailHeader from '../DetailHeader'
+import Details from './details/DetailsApp'
+import Counting from './counting/CountingApp'
 
 const RestaurantDashboardLayout = () => {
   const setDashboardSection = useGeneralDashboardStore(
     (state) => state.setDashboardSection
   )
+
   // Set Dashboard Section
   useEffect(() => {
     setDashboardSection('general')
   }, [])
-  return <App Compo={Compo} />
+  return <DashboardApp Compo={Compo} />
 }
 
 export default RestaurantDashboardLayout
@@ -20,7 +23,11 @@ export default RestaurantDashboardLayout
 const Compo = () => {
   return (
     <VStack flex="1" p="6" bg="#f4f4f4" maxW={{ base: 'full', md: 'full' }}>
+      <RestaurantDetailHeader section={'Restaurante'} />
       <Divider border="5px" />
+      <Details />
+      <Divider border="5px" />
+      <Counting />
     </VStack>
   )
 }

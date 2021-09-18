3cdd4b83e6e9100a5ff19e1e9165dd79ed0e83c2
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-18 15:14:52 -0600

    add default head

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,10 @@
 import { Flex } from '@chakra-ui/react'
+import DefaultHead from '../components/head/DefaultHead'
 
 export default function Home() {
   return (
     <>
-      {/* <DefaultHead>
+      <DefaultHead>
         <link
           href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap"
           rel="stylesheet"
@@ -12,7 +13,7 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&family=Yeseva+One&display=swap"
           rel="stylesheet"
         />
-      </DefaultHead> */}
+      </DefaultHead>
 
       <Flex w="full" minH="100vh" bg="yellow"></Flex>
     </>

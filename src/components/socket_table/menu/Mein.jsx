37bf9cb05979b3fb13786f6925d0598dbbeca6ba
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-17 21:49:11 -0600

    removed topNav

--- src/components/socket_table/menu/Mein.jsx
+++ src/components/socket_table/menu/Mein.jsx
@@ -1,12 +1,10 @@
 import MeinHeader from './MeinHeader'
-import MeinNav from './MeinNav'
 import MeinCategories from './MeinCategories'
 import MeinBody from './MeinBody'
 import DishModal from '../modals/DishModal'
 const Mein = () => {
   return (
     <div>
-      <MeinNav />
       <MeinHeader />
       <MeinCategories />
       <MeinBody />

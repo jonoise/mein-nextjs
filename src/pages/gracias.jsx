79bdb9b6a8dd72b5a8398f655568ed2a7b86fdcd
Amilkar Munoz <yellowballpython@gmail.com>
2021-09-20 21:03:56 -0600

    add thank you page

--- src/pages/gracias.jsx
+++ src/pages/gracias.jsx
@@ -1,5 +1,31 @@
+import { VStack, Heading, Flex, Box, Text } from '@chakra-ui/react'
+import { colors as color } from '../constants'
+import Lottie from 'lottie-react'
+import confettiData from './73862-confetti.json'
 const gracias = () => {
-  return <div></div>
+  return (
+    <Flex
+      justify="center"
+      align="center"
+      w="full"
+      minH="100vh"
+      bg={color.yellow}
+      color={color.black}
+      px={{ base: 2, lg: 5 }}
+    >
+      <VStack maxW="500px">
+        <Heading>¡GRACIAS!</Heading>
+        <Box maxW="200px">
+          <Lottie animationData={confettiData} />
+        </Box>
+        <Heading fontSize="24px">Yey! Ya formas parte del club. 😄</Heading>
+        <Text textAlign="center">
+          Realmente es un honor contar con tu presencia desde una etapa tan
+          temprana del proyecto. Muchas gracias por tu interés! 💓
+        </Text>
+      </VStack>
+    </Flex>
+  )
 }
 
 export default gracias
